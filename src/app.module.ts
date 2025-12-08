f4947feffdb73cfe3c0943c36654df9684fecbfb
valik <valik.0404@mail.ru>
2025-12-08 14:36:54 +0300

    add icon

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,15 @@ import { ConfigModule } from '@nestjs/config';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { path } from 'app-root-path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: `${path}/uploads`,
+      serveRoot: '/uploads',
+    }),
     ConfigModule.forRoot(),
     AuthModule,
     CategoryModule,
